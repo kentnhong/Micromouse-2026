01417294942bd6dc3bfd2cd199ea8e626a3d1378
Kent Hong <kent.n.hong@gmail.com>
2026-01-31 12:37:30 -0800

    Added adc interface

---
+++ common/drivers/io/adc.h
@@ -0,0 +1,40 @@
+/**
+ * @file adc.h
+ * @author Kent Hong
+ * @brief Interface for Analog-to-Digital Converter (ADC) drivers
+ * 
+ */
+
+#pragma once
+
+namespace MM
+{
+/**
+ * @class Adc
+ * @brief Adc driver instance.
+ * 
+ */
+class Adc
+{
+public:
+    /**
+     * @brief Continuously converts analog values to digital values
+     * 
+     * @return True Successful conversion. False otherwise.
+     */
+    virtual bool convert() = 0;
+
+    /**
+     * @brief Reads digital value from ADC buffer
+     * 
+     * @return true Successful read. False otherwise.
+     */
+    virtual bool read() = 0;
+
+    /**
+     * @brief Destroy the pure virtual instance pointing or referring to the Adc object
+     * 
+     */
+    ~Adc() = default;
+};
+};  // namespace MM
