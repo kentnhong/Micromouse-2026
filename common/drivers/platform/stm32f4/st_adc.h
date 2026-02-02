6b9f6328631442d1e712874e50fcafaae11ef909
Kent Hong <kent.n.hong@gmail.com>
2026-02-02 10:53:47 -0800

    Update adc

--- common/drivers/platform/stm32f4/st_adc.h
+++ common/drivers/platform/stm32f4/st_adc.h
@@ -10,7 +10,6 @@
 #include "adc.h"
 #include "stm32f411xe.h"
 
-
 namespace MM
 {
 namespace Stmf4
@@ -19,12 +18,31 @@ struct StAdcParams
 {
     ADC_TypeDef* base_addr;
 };
-class HwAdc
+class HwAdc : public Adc
 {
 public:
     explicit HwAdc(StAdcParams& params);
-    bool convert();
-    bool read();
+
+    /**
+     * @brief Initialize ADC peripheral
+     * 
+     * @return true ADC initialized successfully, false otherwise
+     */
+    bool init();
+
+    /**
+     * @brief Continuous conversion of analog to digital values
+     * 
+     * @return true Conversion successful, false otherwise
+     */
+    bool convert() override;
+
+    /**
+     * @brief Read converted analog values from ADC buffer
+     * 
+     * @return true Read successful, false otherwise
+     */
+    bool read() override;
 };
 };  // namespace Stmf4
 };  // namespace MM
