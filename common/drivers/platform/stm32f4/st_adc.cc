0dbee664dced4cae17759bedb803f4097d24e20e
Kent Hong <kent.n.hong@gmail.com>
2026-02-11 22:09:13 -0800

    first commit

---
+++ common/drivers/platform/stm32f4/st_adc.cc
@@ -0,0 +1,19 @@
+#include "st_adc.h"
+
+namespace MM
+{
+namespace Stmf4
+{
+HwAdc::HwAdc(StAdcParams& params_)
+    : settings{params_.settings}, base_addr{params_.base_addr}
+{
+}
+
+bool HwAdc::init()
+{
+    // Turn on ADC
+    // Start conversion
+    return true;
+}
+};  // namespace Stmf4
+};  // namespace MM
