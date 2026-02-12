4a07332258c3702304cbe21d8a343c319e4175ba
Kent Hong <kent.n.hong@gmail.com>
2026-02-11 22:09:13 -0800

    first commit

--- common/drivers/io/adc.h
+++ common/drivers/io/adc.h
@@ -2,23 +2,11 @@
  * @file adc.h
  * @author Kent Hong
  * @brief Adc driver interface.
-<<<<<<< HEAD
  */
 #pragma once
 
 #include <cstdint>
 
-=======
- * 
- */
-#pragma once
-
-<<<<<<< HEAD
->>>>>>> 0d93109 (first commit)
-=======
-#include <cstdint>
-
->>>>>>> c4336e0 (add dma)
 namespace MM
 {
 /**
@@ -30,7 +18,6 @@ class Adc
 {
 public:
     /**
-<<<<<<< HEAD
     * @brief Continuous conversion of analog to digital values
     * 
     * @return true Conversion successful, false otherwise
@@ -48,25 +35,6 @@ public:
     * @brief Destroy the Adc object
     * 
     */
-=======
-        * @brief Continuous conversion of analog to digital values
-        * 
-        * @return true Conversion successful, false otherwise
-        */
-    virtual bool convert(uint8_t channel) = 0;
-
-    /**
-        * @brief Read converted analog values from ADC buffer
-        * 
-        * @return true Read successful, false otherwise
-        */
-    virtual bool read() = 0;
-
-    /**
-         * @brief Destroy the Adc object
-         * 
-         */
->>>>>>> 0d93109 (first commit)
     ~Adc() = default;
 };
 };  // namespace MM
