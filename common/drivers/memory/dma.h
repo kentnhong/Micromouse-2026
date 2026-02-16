e1f65b6484451de576e11b2f2250ec04d407f58f
Kent Hong <kent.n.hong@gmail.com>
2026-02-16 02:59:00 -0800

    Add params to dma

--- common/drivers/memory/dma.h
+++ common/drivers/memory/dma.h
@@ -6,6 +6,9 @@
  */
 #pragma once
 
+#include <cstdint>
+#include <cstddef>
+
 namespace MM
 {
 class Dma
@@ -13,10 +16,13 @@ class Dma
 public:
     /**
     * @brief Starts DMA transfer from peripheral to memory
+    * @param source Source address
+    * @param destination Destination address
+    * @param num_items Number of data items to be transferred
     * 
     * @return true successful transfer, false otherwise
     */
-    virtual bool start() = 0;
+    virtual bool start(uintptr_t source, uintptr_t destination, size_t num_items) = 0;
 
     /**
     * @brief Stops DMA transfer after it is complete
