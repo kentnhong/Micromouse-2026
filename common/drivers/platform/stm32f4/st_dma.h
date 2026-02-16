2c0a90efd209670abb1517e21c0a154648ef50bc
Kent Hong <kent.n.hong@gmail.com>
2026-02-16 02:59:00 -0800

    Add params to dma

--- common/drivers/platform/stm32f4/st_dma.h
+++ common/drivers/platform/stm32f4/st_dma.h
@@ -13,8 +13,53 @@ namespace MM
 namespace Stmf4
 {
 
+enum class DmaChSel : uint8_t
+{
+    CH0 = 0,
+    CH1,
+    CH2,
+    CH3,
+    CH4,
+    CH5,
+    CH6,
+    CH7
+};
+
+// Add burst transfer config for memory (MBURST) and periph (PBURST)?
+
+enum class DmaWidth : uint8_t
+{
+    BYTE = 0,
+    HALF_WORD,
+    WORD
+};
+
+enum class DmaInc : uint8_t
+{
+    FIXED = 0,
+    INCREMENT
+};
+
+enum class DmaMode : uint8_t
+{
+    NORMAL = 0,
+    CIRCULAR
+};
+
+enum class DmaDataDir : uint8_t
+{
+    PERIPH_TO_MEM = 0,
+    MEM_TO_PERIPH,
+    MEM_TO_MEM
+};
+
 struct StDmaParams
 {
+    DmaChSel channel;
+    DmaWidth width;
+    DmaInc inc;
+    DmaMode mode;
+    DmaDataDir data_dir;
 };
 
 class HwDma : public Dma
@@ -24,10 +69,14 @@ public:
 
     /**
     * @brief Starts DMA transfer from peripheral to memory
+    * @param source Source address
+    * @param destination Destination address
+    * @param num_items Number of data items to be transferred
     * 
     * @return true successful transfer, false otherwise
     */
-    bool start() override;
+    bool start(uintptr_t source, uintptr_t destination,
+               size_t num_items) override;
 
     /**
     * @brief Stops DMA transfer after it is complete
