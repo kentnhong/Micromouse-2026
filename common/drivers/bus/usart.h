45a4c70d423f3213e3221f780ea732e0ef79fb6b
Bex Saw <bex.bcc171@gmail.com>
2026-02-21 20:37:20 -0800

    a build stmf411 usart *wait for sysclk approve & test with main loop

--- common/drivers/bus/usart.h
+++ common/drivers/bus/usart.h
@@ -16,16 +16,16 @@ public:
     /**
     * @brief Receive (Read) data from USART
     * @param data Pointer to buffer where received data will be stored
-    * @param len Length of data to read
+    * @param length Length of data to read
     * @return true if read is successful, false otherwise
     */
-    virtual bool receive(uint8_t* data, size_t len) = 0;
+    virtual bool receive(uint8_t* data, size_t length) = 0;
 
     /**
     * @brief Transfer (send) data to USART
     * @param txbuf Span containing data to be sent
     * @return true if write is successful, false otherwise
     */
-    virtual bool send(std::span<const uint8_t> txbuf) = 0;
+    virtual bool transfer(std::span<const uint8_t> txbuf) = 0;
 };
 }  // namespace MM
