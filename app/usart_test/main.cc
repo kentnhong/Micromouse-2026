b2c05bdc980967e8f2ae0cf0509dee3ebfc15349
Bex Saw <107741592+pasinponsww@users.noreply.github.com>
2026-02-26 12:55:49 -0800

    bs-usart (#28)
    * adding structure & usart skeleton
    
    * a build stmf411 usart *wait for sysclk approve & test with main loop
    
    * skeleton for sysclk driver
    
    * system clock complete
    
    * working sys_clk + still need to test HSE in the future
    
    * usart + need tx fix debug
    
    * Fix usartdiv calculation
    
    * Fix f411 bsp for usart
    
    * Working USART
    
    * Fixed tx buffer variable naming
    
    ---------
    
    Co-authored-by: Kent Hong <kent.n.hong@gmail.com>

---
+++ app/usart_test/main.cc
@@ -0,0 +1,22 @@
+#include <array>
+#include "board.h"
+
+using namespace MM;
+
+std::array<uint8_t, 20> tx_buf{"XuperMouse!\r\n"};
+uint8_t rx_byte;
+
+int main(int argc, char** argv)
+{
+    bsp_init();
+    Board board = get_board();
+
+    while (1)
+    {
+        board.usart.send(tx_buf);
+        // Busy wait
+        for (volatile uint32_t i = 0; i < 1000000; i++);
+    }
+
+    return 0;
+}
