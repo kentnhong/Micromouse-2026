c699771e84f7407d3f8c7354ee74121d96e7f14d
Kent Hong <kent.n.hong@gmail.com>
2026-03-03 13:10:35 -0800

    Add option to enable ADC overrun interrupt

--- app/adc_test/bsp_f411/f411_board.cc
+++ app/adc_test/bsp_f411/f411_board.cc
@@ -7,9 +7,19 @@ bool board_init()
     // Init AHB1 clock
     // Init APB2 clock
     // Init ADC
+    // Init DMA
     return true;
 }
 };  // namespace MM
+
+// Interrupt Handler for ADC overrun
+extern "C" void ADC_IRQHandler()
+{
+}
+
+// Restart adc dma stream
+
+// Start adc dma stream
 
 // // Check for overrun (we are going to reinit the DMA stream, clear ADC OVR, and restart ADC conversions in the BSP via OVR interrupt)
 //     if (base_addr->SR & ADC_SR_OVR)
