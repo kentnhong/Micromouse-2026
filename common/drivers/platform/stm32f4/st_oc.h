90dc7d876c88584a9d86185a2a212a0ba954eac1
Kent Hong <kent.n.hong@gmail.com>
2026-03-19 16:31:22 -0700

    st_oc.h

--- common/drivers/platform/stm32f4/st_oc.h
+++ common/drivers/platform/stm32f4/st_oc.h
@@ -2,6 +2,7 @@
  * @file st_oc.h
  * @author Kent Hong
  * @brief Timer Output Compare Driver API for the STM32F411
+ * @note This driver currently only support TIM2 to TIM5
  */
 
 #pragma once
@@ -12,6 +13,14 @@ namespace MM
 {
 namespace Stmf4
 {
+
+enum class OcDirection : uint8_t
+{
+    UPCOUNTER = 0,
+    DOWNCOUNTER
+};
+
+
 class HwOc : public Oc
 {
 public:
