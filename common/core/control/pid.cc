8f30618f7909c9eda7b2c08af1170087ca31c162
Bex Saw <bex.bcc171@gmail.com>
2026-03-29 11:23:38 -0700

    PID functions + expected math

--- common/core/control/pid.cc
+++ common/core/control/pid.cc
@@ -3,25 +3,55 @@
 namespace MM
 {
 
-static constexpr kMinOutput = -100.0f;  // Replace with actual value later
-static constexpr kMaxOutput = 100.0f;   // Same as above
-
-PID::PID(Bno055Data& imu_data, EncoderInput& enc_data, Val& pid)
+PID::PID(const Val& speed, const Val& turn)
 {
+    // TODO: fetch PID constants from flash and initialize internal state
 }
 
-bool PID::update(const Input& input, const Target& target, Output& output)
+bool PID::update(const Input& input, const Target& target, float dt_sec, MotorOutput& output)
 {
+    // TODO: Implement PID update logic
+    return true;  
 }
 
 bool PID::reset()
 {
+    // TODO: Reset internal state of the PID controller
+    return true;
 }
 
-bool PID::set_output_limits(float min_output, float max_output)
+void PID::set_output_limits(float min_output, float max_output)
 {
-    min_output = kMinOutput;
-    max_output = kMaxOutput;
+    // TODO: Set the output limits for the motor control signals
+}
+
+void PID::set_integral_limit(float integral_limit)
+{
+    // TODO: Set the integral limit to prevent windup
+}
+
+float PID::compute_pid (Loop& loop, float error, float dt_sec)
+{
+    // TODO: Compute the PID control signal based on the error and time step
+    return 0.0f;
+}
+
+float PID::limit_range (float value, float min_value, float max_value)
+{
+    if (value < min_value)
+        return min_value;
+    if (value > max_value)
+        return max_value;
+    return value;   
+}
+
+float PID::limit_angle(float angle)
+{
+    while (angle > 180.0f)
+        angle -= 360.0f;
+    while (angle < -180.0f)
+        angle += 360.0f;
+    return angle;
 }
 
 }  // namespace MM
