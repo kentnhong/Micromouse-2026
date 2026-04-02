579f3d95630669f8869667ef910eedaabafec3ee
Kent Hong <kent.n.hong@gmail.com>
2026-04-01 23:12:52 -0700

    Add chrono to differentiate time units

---
+++ common/drivers/time/oc.h
@@ -0,0 +1,19 @@
+/**
+ * @file oc.h
+ * @author Kent Hong
+ * @brief Timer Output Compare Interface
+ */
+#pragma once
+
+namespace MM
+{
+class Oc
+{
+public:
+    /**
+             * @brief Delete driver object
+             * 
+             */
+    virtual ~Oc() = default;
+};
+};  // namespace MM
