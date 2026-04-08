590e3240402d033bfae4fcf8428c2e60f56df534
Bex Saw <bex.bcc171@gmail.com>
2026-04-08 13:58:56 -0700

    PID without IMU

--- app/pid_controller/board.h
+++ app/pid_controller/board.h
@@ -8,7 +8,7 @@
 #pragma once
 
 #include <tuple>
-#include "bno055_imu.h"
+
 #include "delay.h"
 #include "drv8231.h"
 #include "elapsed.h"
@@ -23,8 +23,6 @@ namespace MM
 
 struct Board
 {
-    Bno055& imu;
-    I2c& i2c;
     Encoder& encoder;
     Pwm& pwm;
     Drv8231& drv8231;
@@ -36,6 +34,6 @@ bool bsp_init(void);
 Board& get_board(void);
 
 /* PID */
-std::tuple<PID&, PID::Input&, PID::MotorOutput&, PID::Target&> get_pid_bundle();
+std::tuple<PID&, PID::MotorOutput&, PID::Target&> get_pid_bundle();
 
 }  // namespace MM
