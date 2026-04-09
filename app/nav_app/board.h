ea30492dc6de9571f3062ecc70ad619483e07520
Bex Saw <bex.bcc171@gmail.com>
2026-04-08 21:45:37 -0700

    navigation

---
+++ app/nav_app/board.h
@@ -0,0 +1,27 @@
+/**
+ * @file board.h
+ * @brief Navigation test board interface
+ * @author Bex Saw
+ * @date 4/8/2026
+ */
+
+#pragma once
+
+#include "bno055_imu.h"
+#include "delay.h"
+#include "gpio.h"
+#include "i2c.h"
+#include "nav.h"
+
+namespace MM
+{
+
+struct Board
+{
+    Bno055& imu;
+};
+
+bool bsp_init(void);
+Board& get_board(void);
+
+}  // namespace MM
