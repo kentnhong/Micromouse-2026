32fdf3d453d974b2e8f40971577c46c20b8ecba3
Bex Saw <bex.bcc171@gmail.com>
2026-04-08 21:45:37 -0700

    navigation

---
+++ common/core/navigation/nav.h
@@ -0,0 +1,27 @@
+/**
+* @file nav.h
+* @author Bex Saw
+* @brief Navigation system for the micromouse using IMU
+* @version 0.1
+* @date 4/8/2026
+*/
+
+#pragma once 
+
+#include "imu_math.h"
+
+/**
+* @note Use IMU yaw to decide when to start/stop a turn, 
+* or to correct the robot’s heading during straight runs
+*/
+
+namespace MM
+{
+class Nav
+{
+public:
+
+private:
+
+};
+}
