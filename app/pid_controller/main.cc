718c91fa31bd3d49a6aee880c377cb31d4d906b2
Bex Saw <bex.bcc171@gmail.com>
2026-04-15 22:05:44 -0700

    refine the PID controller

--- app/pid_controller/main.cc
+++ app/pid_controller/main.cc
@@ -11,11 +11,6 @@
 
 using namespace MM;
 
-/**
-* @note For this test only care about the PID controller and its output to the motor driver.
-* The trapezoidal velocity profile is not used in this test, but will be part of the motion 
-* API test later on. Once this is verified.
-*/
 int main(int argc, char* argv[])
 {
     bsp_init();
@@ -23,36 +18,41 @@ int main(int argc, char* argv[])
 
     auto [pid, output, target] = get_pid_bundle();
 
-    // PWM duty cycles for left and right motors
     uint8_t left_pwm = 0;
     uint8_t right_pwm = 0;
 
-    hw.encoder.reset_ticks();
-    hw.drv8231.set_direction(Drv8231::Direction::FORWARD);
+    // Get the encoder timing configuration based on the desired sample time
+    const Sample::EncoderTiming encoder_timing =
+        Sample::init_encoder_timing(hw.left_encoder, hw.encoder_sample_us);
+
+    // Encoder Reset
+    hw.left_encoder.reset_ticks();
+    hw.right_encoder.reset_ticks();
+
+    // Direction setup
+    hw.left_motor.set_direction(Drv8231::Direction::FORWARD);
+    hw.right_motor.set_direction(Drv8231::Direction::FORWARD);
 
     while (1)
     {
-        int32_t ticks = hw.encoder.get_ticks();
+        EncoderInput encoder = Sample::sample_encoders(
+            hw.left_encoder, hw.right_encoder, encoder_timing);
 
-        EncoderInput encoder;
-        encoder.left_ticks = ticks;
-        encoder.right_ticks = ticks;
+        // Update the PID controller with the encoder reading and target velocity
+        if (!pid.update(encoder, target, encoder_timing.sample_time_sec,
+                        output))
+        {
+            hw.left_motor.set_speed(0);
+            hw.right_motor.set_speed(0);
+            continue;
+        }
 
-        // IMU removed: yaw now only from encoder or setpoint
-
-        pid.update(encoder, target, 0.0f, output);
-
+        // Convert the PID output to PWM duty cycle values for the left and right motors
         pid.output_to_pwm_duty_cycle(output, left_pwm, right_pwm);
 
-        /* Average the left and right PWM values for a single duty cycle 
-           since the driver controls both motors together */
-        const uint8_t duty_cycle =
-            static_cast<uint8_t>((left_pwm + right_pwm) / 2);
-
-        hw.drv8231.set_speed(duty_cycle);
-
-        hw.encoder.reset_ticks();
-        Utils::DelayMs(10);
+        // Set the motor speeds based on the calculated PWM duty cycles
+        hw.left_motor.set_speed(left_pwm);
+        hw.right_motor.set_speed(right_pwm);
     }
 
     return 0;
