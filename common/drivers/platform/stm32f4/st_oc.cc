489f4e33f0f3f54f252e12a824330ce027d2beb3
Kent Hong <kent.n.hong@gmail.com>
2026-04-20 00:17:15 -0700

    Add pseudocode for init

--- common/drivers/platform/stm32f4/st_oc.cc
+++ common/drivers/platform/stm32f4/st_oc.cc
@@ -14,18 +14,21 @@ bool HwOc::init(uint32_t init_timer_freq)
     // Enable Auto-reload preload enable bit (ARPE) in TIMx_CR1 register
     // Enable Edge-aligned mode from clearing CMS in TIMx_CR1
     // Set as upcounter by clearing DIR in TIMx_CR1
-
-    // Generate update events on overflow by setting the UG bit in the TIMx_EGR
+    // Do not set UIF flag for interrupt and DMA requests (enable URS in TIMx_CR1)
 
     // Set current timer_freq
     this->set_freq(init_timer_freq);
+
+    // Start counter (set CEN in TIMx_CR1)
+
     return true;
 }
 
 bool HwOc::set_freq(uint32_t new_timer_freq)
 {
     // Disable update event so it doesn't conflict with auto reload val being written to shadow register (set UDIS in TIMx_CR1)
-    // Remember to enable update event after setting frequency
+    // Set new timer freq in between here
+    // Remember to enable update event after setting frequency (clear UDIS)
 
     // Set current timer frequency
     timer_freq = new_timer_freq;
