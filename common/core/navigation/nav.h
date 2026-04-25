69d9adfba4f36947cbd018ae3c69617313776f8e
Bex Saw <bex.bcc171@gmail.com>
2026-04-25 14:46:15 -0700

    initial commit

--- common/core/navigation/nav.h
+++ common/core/navigation/nav.h
@@ -9,6 +9,7 @@
 #pragma once 
 
 #include "imu_math.h"
+#include "bno055.h"
 
 /**
 * @note Use IMU yaw to decide when to start/stop a turn, 
@@ -20,6 +21,15 @@ namespace MM
 class Nav
 {
 public:
+
+    // Constructor
+    Nav();
+
+    // Having a function where it holds the statemachine for the navigation control
+
+    // Function that checking the imu data and decide when the start/stop a turn (FOR OVERHEADING)
+
+    ~Nav();
 
 private:
 
