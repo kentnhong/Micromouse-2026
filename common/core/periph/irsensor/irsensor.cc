3f29dc490023d21d1d126204b34a918a4808ab8c
Kent Hong <kent.n.hong@gmail.com>
2026-04-30 18:27:51 -0700

    Add states, constructor, function headers, and member variables for IrSensor class

--- common/core/periph/irsensor/irsensor.cc
+++ common/core/periph/irsensor/irsensor.cc
@@ -1,1 +1,16 @@
 #include "irsensor.h"
+
+namespace MM
+{
+IrSensor::IrSensor(IrParams params_)
+    : adc{params_.adc}, dma{params_.dma}, emitter{params_.emitter}
+{
+}
+
+bool IrSensor::update()
+{
+    // TODO: Complete this
+    return true;
+}
+
+};  // namespace MM
--- common/core/periph/irsensor/irsensor.h
+++ common/core/periph/irsensor/irsensor.h
@@ -1,20 +1,78 @@
 /**
  * @file irsensor.h
  * @author Kent Hong
- * @brief IR Sensor interface for Xupermouse
+ * @brief IR Sensor state machine for Xupermouse
  */
 
 #pragma once
 
+#include <array>
+#include <cstdint>
 #include "adc.h"
 #include "dma.h"
 #include "gpio.h"
 
 namespace MM
 {
+
+enum class IrStates : uint8_t
+{
+    SAMPLE_OFF_1 = 0,
+    SAMPLE_OFF_2,
+    EMITTER_ON,
+    SETTLE_1,
+    SAMPLE_ON_1,
+    SAMPLE_ON_2,
+    EMITTER_OFF,
+    SETTLE_2
+};
+
+struct IrParams
+{
+    Adc& adc;
+    Dma& dma;
+    Gpio& emitter;
+};
+
 class IrSensor
 {
 public:
+    /**
+     * @brief Construct a new Ir Sensor object
+     * 
+     * @param params_ Struct with ADC, DMA, and GPIO objects to control the IR Sensor
+     */
+    explicit IrSensor(IrParams params_);
+
+    /**
+     * @brief Execute current IR Sensor state and move to next state
+     * 
+     * @return true State executed and incremented successfully, false otherwise.
+     */
+    bool update();
+
+    /**
+     * @brief Default destructor
+     * 
+     */
+    ~IrSensor() = default;
+
 private:
+    /**
+     * @brief Calculates true ADC value after taking the averages of the ambient and combined values then subtracting them
+     *        ((combined[0] + combined[1]) / 2) - ((ambient[0] + ambient[1]) / 2)
+     * 
+     * @return true ADC value calculated successfully, false otherwise
+     */
+    bool calculate();
+
+    IrStates current_state = IrStates::SAMPLE_OFF_1;
+    Adc& adc;
+    Dma& dma;
+    Gpio& emitter;
+    std::array<uint16_t, 2> ambient;  // ADC samples with ambient light
+    std::array<uint16_t, 2>
+        combined;           // ADC sample with ambient light + true IR
+    uint16_t adc_true = 0;  // Final calculated ADC value with just true IR
 };
 };  // namespace MM
