71a1fdcee86d761ff841eeb8348b7370921d74b5
Kent Hong <kent.n.hong@gmail.com>
2026-04-30 17:58:54 -0700

    Initial commit

---
+++ common/core/periph/irsensor/irsensor.cc
@@ -0,0 +1,1 @@
+#include "irsensor.h"
---
+++ common/core/periph/irsensor/irsensor.h
@@ -0,0 +1,20 @@
+/**
+ * @file irsensor.h
+ * @author Kent Hong
+ * @brief IR Sensor interface for Xupermouse
+ */
+
+#pragma once
+
+#include "adc.h"
+#include "dma.h"
+#include "gpio.h"
+
+namespace MM
+{
+class IrSensor
+{
+public:
+private:
+};
+};  // namespace MM
