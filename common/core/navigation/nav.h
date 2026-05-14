690b92585d7286114332ea6da293b99f126cd1ea
Bex Saw <bex.bcc171@gmail.com>
2026-05-13 18:31:42 -0700

    restructure the nav w/o nav

--- common/core/navigation/nav.h
+++ common/core/navigation/nav.h
@@ -1,49 +1,54 @@
-/**
-* @file nav.h
-* @author Bex Saw
-* @brief Navigation system for the micromouse using IMU
-* @version 0.1
-* @date 4/8/2026
-*/
-
 #pragma once
+#include "floodfill.h"
 
-#include "bno055_imu.h"
-#include "imu_math.h"
-// #include "floodfill.h"
 // #include "ir_interface.h"
+
+/**
+* @note Inherit from the IR interface class to get access to the latest 
+* IR sensor data for decision making.
+*
+* @note The wholepoint of the Navigation control is for the "floodfill"
+* To knows the next move, and for the Navigation class to execute that move
+*/
 
 namespace MM
 {
+
 class Navigation
 {
-
     enum class State : uint8_t
     {
         STRAIGHT = 0,
         TURNING_LEFT,
         TURNING_RIGHT,
+        U_TURN,
         STOPPED,
         RESET
     };
 
 public:
-    Navigation();
+    explicit Navigation();
 
     /**
-    * @brief Update the navigation system with new IMU data
-    * @param imu The latest IMU data from the BNO055 sensor
+    * @brief Update the navigation state based on the latest IMU and IR sensor data.
+    * @param imu The latest IMU data, used for precise movement control.
+    * @param ir The latest IR sensor data, used for maze-solving decisions.
     */
-    void update(const Bno055Data& imu);
+    void update(const Bno055Data& imu, /*const IrData& ir*/);
 
-    /**
-    * @brief Check if a turn is needed based on IMU data
-    */
-    void check_turn();
+    State get_current_state() const
+    {
+        return state;
+    }
 
     ~Navigation();
 
 private:
     State state;
+    Floodfill floodfill;
+
+    // Flag for decision phase; true when we are ready to make a new move
+    bool at_cell_center{true};
 };
+
 }  // namespace MM
