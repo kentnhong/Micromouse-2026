7220ea717aabeb96fe0204de3322f41ddbd11bed
Bex Saw <bex.bcc171@gmail.com>
2026-05-17 14:41:54 -0700

    adding motion api; need to review pid b4 merge; nav restructure to support ircontroller

---
+++ common/core/.api/motioncontroller.cc
@@ -0,0 +1,32 @@
+#include "motioncontroller.h"
+
+namespace MM
+{
+
+bool MotionController::forward(const IrValues& ir)
+{
+
+    return false;
+}
+
+bool MotionController::turn_left()
+{
+
+    return false;
+}
+
+bool MotionController::turn_right()
+{
+    return false;
+}
+
+bool MotionController::u_turn()
+{
+    return false;
+}
+
+void MotionController::stop()
+{
+}
+
+}  // namespace MM
