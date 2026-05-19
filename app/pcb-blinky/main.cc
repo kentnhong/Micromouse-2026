43f960a29d6c85098a72427080285f4daa7d3ef0
Bex Saw <bex.bcc171@gmail.com>
2026-05-18 21:18:03 -0700

    blinky on pcb

---
+++ app/pcb-blinky/main.cc
@@ -0,0 +1,24 @@
+#include <cstdlib>
+#include "board.h"
+#include "gpio.h"
+
+using namespace MM;
+
+int main(int argc, char* argv[])
+{
+    board_init();
+    Board hw = get_board();
+    while (1)
+    {
+        hw.led_a.toggle();
+        for (volatile size_t i = 0; i < 100000; i++);  // Delay
+
+        hw.led_b.toggle();
+        for (volatile size_t i = 0; i < 100000; i++);  // Delay
+
+        hw.led_c.toggle();
+        for (volatile size_t i = 0; i < 100000; i++);  // Delay
+    }
+
+    return 0;
+}
